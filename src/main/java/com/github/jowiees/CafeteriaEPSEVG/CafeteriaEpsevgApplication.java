7fda8b0eedec1d4bc802fe113a27776a88c0550a
Joel Esteller <joelestepla@gmail.com>
2025-12-07 00:03:06 +0100

    dependency problem

---
+++ src/main/java/com/github/jowiees/CafeteriaEPSEVG/CafeteriaEpsevgApplication.java
@@ -0,0 +1,13 @@
+package com.github.jowiees.CafeteriaEPSEVG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaEpsevgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteriaEpsevgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jowiees/CafeteriaEPSEVG/CafeteriaEpsevgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jowiees.CafeteriaEPSEVG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteriaEpsevgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
